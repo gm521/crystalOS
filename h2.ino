a4dc8c45662ad6f98fc8b9f4d2e23214d71a4fe8
GamingGamer521 <134823809+gm521@users.noreply.github.com>
2024-10-11 07:20:58 -0400

    Update h2.ino

--- h2.ino
+++ h2.ino
@@ -10,26 +10,32 @@
 #include <3bc.h>
 SdFat sd;
 struct app_3bc_s* h = lang_3bc_init();
+
+char h2c(const char *h, int o) {
+    return ((h[o] >= 'a' ? h[o] - 87 : h[o] - 48) << 4) |
+    (h[o + 1] >= 'a' ? h[o + 1] - 87 : h[o + 1] - 48);
+}
+
 void setup() {
   Serial.begin(9600);
   lang_3bc_print(h, tty_output, [](char* str){
     Serial.write(str);    
   });
-  char O[196] = "";
+  char O[196];
   sd.begin(10, SD_SCK_MHZ(4));
   File32 f = sd.open("prg.v");
   while (f.available()) {
     char c = f.read();
     strncat(O, &c, 1);
   }
-  char* ln = strtok(O, "\n");
+  char* ln = strtok(O, ";");
   while (ln != nullptr) {
-    char a0 = (10000 * (ln[0x0] - 48)) + (1000 * (ln[0x1] - 48)) + (100 * (ln[0x2] - 48)) + (10 * (ln[0x3] - 48)) + (ln[0x4] - 48);
-    char a1 = (10000 * (ln[0x5] - 48)) + (1000 * (ln[0x6] - 48)) + (100 * (ln[0x7] - 48)) + (10 * (ln[0x8] - 48)) + (ln[0x9] - 48);
-    char a2 = (10000 * (ln[0xa] - 48)) + (1000 * (ln[0xb] - 48)) + (100 * (ln[0xc] - 48)) + (10 * (ln[0xd] - 48)) + (ln[0xe] - 48);
+    char a0 = h2c(ln, 0);
+    char a1 = h2c(ln, 2);
+    char a2 = h2c(ln, 4);
     
     lang_3bc_line(h, a0, a1, a2);
-    ln = strtok(nullptr, "\n");
+    ln = strtok(nullptr, ";");
   }
 }
 void loop() {
