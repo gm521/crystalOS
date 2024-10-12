2917a07de32fde0e1dc7be5f1da50bc652e2537b
GamingGamer521 <134823809+gm521@users.noreply.github.com>
2024-10-12 02:17:21 -0400

    Update h2.ino

--- h2.ino
+++ h2.ino
@@ -1,41 +1,43 @@
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-             HYDROGEN-2
-          By GamingGamer521
+              CRYSTAL OS
+           By GamingGamer521
+
+          h2 (Hydrogen-2) BIOS
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
-// - Used in Crystal OS, an operating system for the arduino nano
-// - Used in other personal projects of mine
 #include <SdFat.h>
+#include <Wire.h>
+#include "ssd1306.h"
+#include "ssd1306_console.h"
 #define _3BC_DISABLE_INTERPRETER
+#define TBC_CUSTOM
 #include <3bc.h>
 SdFat sd;
 struct app_3bc_s* h = lang_3bc_init();
-
-char h2c(const char *h, int o) {
-    return ((h[o] >= 'a' ? h[o] - 87 : h[o] - 48) << 4) |
-    (h[o + 1] >= 'a' ? h[o + 1] - 87 : h[o + 1] - 48);
-}
+Ssd1306Console t;
 
 void setup() {
-  Serial.begin(9600);
+  ssd1306_128x64_i2c_init();
+  ssd1306_clearScreen();
+  ssd1306_setFixedFont(ssd1306xled_font6x8);
   lang_3bc_print(h, tty_output, [](char* str){
-    Serial.write(str);    
+    t.print(str);
   });
-  char O[196];
+  lang_3bc_custom(h, 1, 1,
+  [](register_3bc_t reg, address_3bc_t addr, data_3bc_t chr) {
+    t.print("ok");
+  });
+  t.println("ok");
+  char O[255];
   sd.begin(10, SD_SCK_MHZ(4));
   File32 f = sd.open("prg.v");
   while (f.available()) {
     char c = f.read();
     strncat(O, &c, 1);
   }
-  char* ln = strtok(O, ";");
-  while (ln != nullptr) {
-    char a0 = h2c(ln, 0);
-    char a1 = h2c(ln, 2);
-    char a2 = h2c(ln, 4);
-    
-    lang_3bc_line(h, a0, a1, a2);
-    ln = strtok(nullptr, ";");
+  for (int i = 0; O[i] != 0; i += 4) {
+    lang_3bc_line(h, MODE, NILL, O[0 + i]-16);
+    lang_3bc_line(h, O[1 + i]-16, O[2 + i]-16, O[3 + i]-16);
   }
 }
 void loop() {
